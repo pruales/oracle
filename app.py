727cd0db667daa0da79f34c4e6a3577ffd859d67
Paul Ruales <paulruales1@gmail.com>
2020-02-03 20:45:47 -0800

    wip add document

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from services.typesense import TypeSense
+
 app = Flask(__name__)
 
 @app.route('/health', methods=['GET'])
@@ -14,11 +15,7 @@ def search():
     data = request.get_json()
     search_params = data['search_params']
     collection = data['collection']
-    # search_parameters = {
-    #     'q'         : 'harry',
-    #     'query_by'  : 'title',
-    #     'sort_by'   : 'ratings_count:desc'
-    # }
+
     print(search_params)
     print(collection)
     result = client.search(collection, search_params)
@@ -30,7 +27,9 @@ def create_collection():
     result = client.create_collection(schema)
     return jsonify(result), 200
 
-@app.route('')
+@app.route('/create/document', methods=['POST'])
+def create_document():
+    
 
 if __name__ == '__main__':
     client = TypeSense()
